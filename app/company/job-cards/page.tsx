291280ae3b05c0da60bd9f472467b1117ff9bbc0
ThabeloPeter <thabelopetertshivhidzo@gmail.com>
2025-11-04 12:02:05 +0200

    Remove duplicate empty state code from job cards page

--- app/company/job-cards/page.tsx
+++ app/company/job-cards/page.tsx
@@ -285,10 +285,6 @@ export default function JobCardsPage() {
             ))
           )}
         </div>
-            <ClipboardList className="w-12 h-12 text-gray-400 dark:text-gray-600 mx-auto mb-4" />
-            <p className="text-gray-600 dark:text-gray-400">No job cards found</p>
-          </div>
-        )}
       </main>
 
       {/* Create Job Card Modal */}
